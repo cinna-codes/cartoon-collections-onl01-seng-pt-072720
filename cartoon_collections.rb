226b25eac37291bf9650b06e1cdc9c27ae4524a4
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-05 18:25:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,12 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map do |element|
+    element = element.capitalize << "!"
+    puts element
+  end
 end
 
 def long_planeteer_calls# code an argument here
