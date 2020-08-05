cc8f45d460de40ecfd48fa7816bc6a77f4c19471
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-05 19:03:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(words)# code an argument here
   words.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find(|che| che = cheese_types)
 end
