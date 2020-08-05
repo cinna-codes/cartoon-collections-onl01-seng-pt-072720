4c956c7c039545c22244aab957887676601342aa
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-05 18:15:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index.map do |dwarf|
+    puts
+  end
 end
 
 def summon_captain_planet# code an argument here
